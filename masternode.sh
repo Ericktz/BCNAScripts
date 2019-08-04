2ed6cfc0083cd01533e4d3fc4501a4ef17423171
hellrezistor <50468493+hellresistor@users.noreply.github.com>
2019-08-05 00:30:53 +0100

    Update masternode.sh

--- masternode.sh
+++ masternode.sh
@@ -3,18 +3,44 @@
 # To Ubuntu Server 18.04
 # by DoMato
 # Updating
-echo "Run as root!"
+# Donate BCNA: B9bMDqgoAY7XA5bCwiUazdLKA78h4nGyfL
+#
 
-#Add bitcanna user
-sudo adduser bitcanna
-sudo visudo 
-sudo cp /etc/sudoers /etc/oldsudoers
+check(){
+echo "Checking as running as root user"
+sleep 1
+if [ "root" != "$USER" ]
+  then  sudo su -c "$0" root
+  exit
+fi
+}
+
+checkapt(){
+echo "Checking packages.."
+apt update
+apt upgrade -y
+apt install -y unzip
+}
+
+userad(){
+adduser bitcanna
+cp /etc/sudoers /etc/oldsudoers
 sed '/root    ALL=(ALL:ALL) ALL/a bitcanna  ALL=(ALL:ALL) ALL' file /etc/sudoers
-su bitcanna
-cd /home/bitcanna
+}
+
+check
+checkapt
+userad
+
+su bitcanna && cd ~
 wget https://github.com/BitCannaGlobal/BCNA/releases/download/1.0.0/bcna-1.0.0-unix.zip
 mkdir Bitcanna
-mkdir .bitcanna
 unzip -xfz bcna-1.0.0-unix.zip -d Bitcanna
-cd Bitcanna
-sudo ./bitcannad
+#cd Bitcanna
+.~/Bitcanna/bitcannad --daemon && sleep 2 && .~/Bitcanna/bitcanna-cli stop
+
+rm .bitcanna/masternode.conf
+.~/Bitcanna/bitcannad --daemon
+
+echo "WAIT TO SYNC..."
+# FOR cicle to check syncing --> .~/Bitcanna/bitcanna-cli 
